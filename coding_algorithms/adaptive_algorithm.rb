1994d080398859f727b1a059614775640d7ad6ce
Alina Moskieva <alina.moskieva@flatstack.com>
2017-12-01 11:52:53 +0300

    finalize adaptive and quality fixes

--- coding_algorithms/adaptive_algorithm.rb
+++ coding_algorithms/adaptive_algorithm.rb
@@ -1,5 +1,6 @@
 class AdaptiveAlgorithm
-  attr_accessor :input, :alphabet, :weight, :probabilities, :file_name
+  attr_accessor :input, :alphabet, :weight, :probabilities, :file_name, :similars
+  RESULT_TITLE = "Bits divided by the number of symbols ".freeze
 
   def initialize(file_name)
     @file_name = file_name
@@ -7,6 +8,8 @@ class AdaptiveAlgorithm
     @alphabet = determine_alphabet.freeze
     @weight = init_weight
     @probabilities = recalculate_probabilities
+    @similars = "0."
+    @symbols_amount = 0
   end
 
   def encode_text
@@ -16,42 +19,69 @@ class AdaptiveAlgorithm
     File.open(file_name, "r") do |f|
       f.each_char do |symbol|
         segment = order_segments(range)[symbol]
-        range =  determine_range(segment)
+        range = [segment[:right], segment[:left]]
         update_weight(symbol)
         recalculate_probabilities
+        increment_symbols_amount
       end
     end
-    code = rand * (range.last - range.first) + range.first
-    write_answer(range, code)
-    
+    write_answer(range)
   end
 
   private
-  def write_answer(range, code)
+
+  def increment_symbols_amount
+    @symbols_amount += 1
+  end
+
+  def write_answer(range)
+    code = code_formition(range)
     puts "Range is #{range}"
-    puts "Code id #{code}"
+    puts "Code is #{code}"
+    puts_answer_in_a_file(range, code)
   end
-  
-  def determine_range(segment)
-    [segment[:right], segment[:left]]
+
+  def puts_answer_in_a_file(range, code)
+    out = File.new("adaptive_coding_algorithm.bin", "w")
+    out.write(binary_code(code))
+    puts "#{RESULT_TITLE} #{(out.size / @symbols_amount.to_f).round(8)}"
   end
-  
+
+  def code_formition(range)
+    code = (rand * (range.last - range.first) + range.first).to_s
+    similars << code[2..code.length]
+  end
+
   def update_weight(symbol)
     weight[symbol] = weight[symbol] + 1
   end
 
   def order_segments(range)
-    right, left = range
+    right, left = determine_range(range)
     length = left - right
     segments = {}
-    
+
     probabilities.each do |key, value|
       left = right + length * value
       segments[key] = { right: right, left: left }
       right = left
     end
-    
+
     segments
+  end
+
+  def determine_range(range)
+    right, left = range
+    return range unless (left * 10).to_i == (right * 10).to_i
+
+    determine_range(recalculate_range(left, right))
+  end
+
+  def recalculate_range(left, right)
+    similar = (left * 10).to_i
+    similars << similar.to_s
+
+    [right * 10 - similar, left * 10 - similar]
   end
 
   def recalculate_probabilities
@@ -70,6 +100,11 @@ class AdaptiveAlgorithm
     alphabet.each { |symbol| weight[symbol] = 1 }
     weight
   end
+
+  def binary_code(code)
+    code[2..-1].to_i.to_s(2)
+  end
 end
 
+# AdaptiveAlgorithm.new("../entropy/souls.txt").encode_text
 AdaptiveAlgorithm.new("ex.txt").encode_text
--- coding_algorithms/arithmetic_coding_algorithm.rb
+++ coding_algorithms/arithmetic_coding_algorithm.rb
@@ -2,14 +2,15 @@ require_relative "probability"
 require_relative "encode"
 
 class ArithmeticCodingAlgorithm
-  attr_accessor :probabilities, :file_name
-  RESULT_TITLE = "Bits divided by the number of symbols "
+  attr_accessor :probabilities, :file_name, :similars
+  RESULT_TITLE = "Bits divided by the number of symbols ".freeze
 
   def initialize(file_name)
     @file_name = file_name
     probability = Probability.new(file_name)
     @probabilities = probability.symbols_probabilities
     previous_results(probability)
+    @similars = "0."
     puts "Arithmetic coding algorithm"
   end
 
@@ -21,53 +22,62 @@ class ArithmeticCodingAlgorithm
       f.each_char do |symbol|
         segment = order_segments(range)[symbol]
         range = [segment[:right], segment[:left]]
-        symbols_amount = symbols_amount + 1
+        symbols_amount += 1
       end
     end
-    
+
     answer(determine_range(range), symbols_amount)
   end
-  
+
   private
-  
+
   def answer(range, symbols_amount)
-    code = rand * (range.last - range.first) + range.first
+    code = code_formition(range)
     puts "Range is #{range.inspect}"
-    puts "Code is #{code}"
-    puts "Symbol amount is #{symbols_amount}"
-    
+    # puts "Code is #{code}"
+    puts "Symbols amount is #{symbols_amount}"
+    puts "Code length is #{code.length}"
+
     write_answer(symbols_amount, code)
   end
-  
+
+  def code_formition(range)
+    code = (rand * (range.last - range.first) + range.first).to_s
+    similars << code[2..code.length]
+  end
+
   def write_answer(symbols_amount, code)
     out = File.new("arithmetic_coding_algorithm.bin", "w")
-    out.write(code)
-    # puts "#{RESULT_TITLE} #{(out.size / symbols_amount.to_f).round(8) }"
+    out.write(binary_code(code))
+    puts "#{RESULT_TITLE} #{(out.size / symbols_amount.to_f).round(8)}"
   end
 
   def order_segments(range)
     right, left = determine_range(range)
     length = left - right
     segments = {}
-    
+
     probabilities.each do |key, value|
       left = right + length * value
       segments[key] = { right: right, left: left }
       right = left
     end
-    
+
     segments
   end
-  
+
   def determine_range(range)
     right, left = range
 
-    return range unless (left * 10 ).to_i == (right * 10 ).to_i
-    
-    similar = (left * 10 ).to_i
-    left = left * 10 - similar
-    right = right * 10 - similar
-    determine_range [right, left]
+    return range unless (left * 10).to_i == (right * 10).to_i
+    determine_range(recalculate_range(left, right))
+  end
+
+  def recalculate_range(left, right)
+    similar = (left * 10).to_i
+    similars << similar.to_s
+
+    [right * 10 - similar, left * 10 - similar]
   end
 
   def previous_results(probability)
@@ -75,6 +85,11 @@ class ArithmeticCodingAlgorithm
     Encode.new(file_name).encode("ShannonFano")
     Encode.new(file_name).encode("Haffman")
   end
+
+  def binary_code(code)
+    code[2..-1].to_i.to_s(2)
+  end
 end
 
-ArithmeticCodingAlgorithm.new("../entropy/souls.txt").encode_text
+# ArithmeticCodingAlgorithm.new("../entropy/souls.txt").encode_text
+ArithmeticCodingAlgorithm.new("ex.txt").encode_text
